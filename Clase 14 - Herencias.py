4d1cc0330b3cff21646b1415c424e581cc5d95c6
Facundo Ezequiel Zárate <facundoezequielzarate@gmail.com>
2022-09-13 19:02:27 -0300

    actualizacion 2

--- Clase 14 - Herencias.py
+++ Clase 14 - Herencias.py
@@ -3,14 +3,27 @@ class AnimalMarino:
         self.especie = especie
         self.apendices = apendices
     
-    def presentacion(self):
-        print(f"Soy un {self.especie} y tengo {self.apendices} apéndices") # no entiendo por qué me arroja un error entre el print y el paréntesis
-
 class Mamifero(AnimalMarino):
-    pass
+    def presentacion(self):
+        print(f"Soy un {self.especie} y tengo {self.apendices} apéndices")
 
 class Cetaceo(AnimalMarino):
-    pass
+    def presentacion(self):
+        print(f"Soy un {self.especie} y tengo {self.apendices} apéndices")
+
+
+class Foca(Mamifero):
+    def __init__(self, animal):
+        self.animal = animal
+        print(f"Soy una {self.aniaml} y {presentacion}") # Acá quiero invocar a la función de la línea 6. Es posible o ya estoy en cualquiera?
+
+
+class Marsopa(Cetaceo):
+    def __init__(self, animal):
+        self.animal = animal
+        print(f"Soy una {self.aniaml} y {presentacion}")
+
+
 
 mi_foca = Mamifero("Mamífero", "Tres")
 mi_marsopa = Cetaceo("Cetaceo", "Cuatro")
