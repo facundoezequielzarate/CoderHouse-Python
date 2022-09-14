8383c693582f683c423af6645d724f589c1d1ff2
Facundo Ezequiel Zárate <facundoezequielzarate@gmail.com>
2022-09-14 19:10:47 -0300

    2.1

--- main-herencias.py
+++ main-herencias.py
@@ -1,15 +1,33 @@
-class AnimalesMarinos:
+class AnimalesConPelaje:
     def __init__(self, pelaje):
         self.pelaje = pelaje
 
     def presentarse(self):
-        print(f"Hola soy una {self.nombre_lindo} y mi pelaje es {self.pelaje}")
+        print(f"Hola soy {self.nombre_lindo} y mi pelaje es {self.pelaje}")
 
-class Foca(AnimalesMarinos):
-    nombre_lindo = "Foca"
 
-class Pinguino(AnimalesMarinos):
-    nombre_lindo = "Pingüino"
+class AnimalesConPlumaje:
+    def __init__(self, plumaje):
+        self.plumaje = plumaje
+
+    def presentarse(self):
+        print(f"Hola soy {self.nombre_lindo} y mi plumaje es {self.plumaje}")
+
+
+class Foca(AnimalesConPelaje):
+    nombre_lindo = "una Foca"
+
+
+class Pinguino(AnimalesConPlumaje):
+    nombre_lindo = "un Pingüino"
+
+
+class Caballo(AnimalesConPelaje):
+    nombre_lindo = "un Caballo"
+
+
+class Condor(AnimalesConPlumaje):
+    nombre_lindo = "un Cóndor"
 
 
 mi_foca = Foca("gris")
@@ -24,3 +42,13 @@ mi_pinguino_tricolor = Pinguino("negro, blanco y amarillo")
 
 mi_pinguino.presentarse()
 mi_pinguino_tricolor.presentarse()
+
+mi_caballo = Caballo("gris")
+mi_caballo_blanco = Caballo("blanco")
+
+mi_caballo.presentarse()
+mi_caballo_blanco.presentarse()
+
+
+mi_condor = Condor("negro y blanco")
+mi_condor.presentarse()
--- main.py
+++ main.py
@@ -1,24 +1,51 @@
-class AnimalesMarinos:
-    def __init__(self, pelaje):
-        self.pelaje = pelaje
-    
-class Foca(AnimalesMarinos):
-    nombre_lindo = "Foca"
-
+class AnimalesConPelaje:
     def __init__(self, pelaje):
         self.pelaje = pelaje
 
     def presentarse(self):
-        print(f"Hola soy una {self.nombre_lindo} y mi pelaje es {self.pelaje}")
-    
-    def __str__(self):
-        return str(type(self))
+        print(f"Hola soy {self.nombre_lindo} y mi pelaje es {self.pelaje}")
 
-class Pinguino:
-    su_nombre = "Pingüino"
+class AnimalesConPlumaje:
+    def __init__(self, plumaje):
+        self.plumaje = plumaje
+
+    def presentarse(self):
+        print(f"Hola soy {self.nombre_lindo} y mi plumaje es {self.plumaje}")
+
+
+class Foca(AnimalesConPelaje):
+    nombre_lindo = "una Foca"
+
+
+class Pinguino(AnimalesConPlumaje):
+    nombre_lindo = "un Pingüino"
+
+class Caballo(AnimalesConPelaje):
+    nombre_lindo = "un Caballo"
+
+class Condor(AnimalesConPlumaje):
+    nombre_lindo = "un Cóndor"
 
 
 mi_foca = Foca("gris")
 mi_foca_marron = Foca("marron")
+
 mi_foca.presentarse()
 mi_foca_marron.presentarse()
+
+
+mi_pinguino = Pinguino("negro y blanco")
+mi_pinguino_tricolor = Pinguino("negro, blanco y amarillo")
+
+mi_pinguino.presentarse()
+mi_pinguino_tricolor.presentarse()
+
+mi_caballo = Caballo("gris")
+mi_caballo_blanco = Caballo("blanco")
+
+mi_caballo.presentarse()
+mi_caballo_blanco.presentarse()
+
+
+mi_condor = Condor("negro y blanco")
+mi_condor.presentarse()
